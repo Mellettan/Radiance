d5c14fe0554d964857e7890a6f7a0dd68670475f
Mellettan <roma23470@gmail.com>
2024-08-24 05:02:45 +0300

    test: add tests for commands for cron jobs
    Add tests for  commands for create and remove cron jobs for `schedule_daily_posts`

--- posts/management/commands/add_cron_job.py
+++ posts/management/commands/add_cron_job.py
@@ -5,18 +5,44 @@ from pathlib import Path
 
 
 class Command(BaseCommand):
+    """
+    Добавляет CRON задачу для планирования ежедневных постов ботами.
+
+    Этот класс представляет команду управления Django, которая добавляет
+    CRON задачу для выполнения команды Django по расписанию. Команда
+    запускает задачу `schedule_daily_posts` каждый день в полночь.
+
+    Attributes:
+        help (str): Описание команды для вывода в справке.
+    """
+
     help = 'Adds a CRON job to schedule daily posts for bots'
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **kwargs) -> None:
+        """
+        Выполняет команду добавления CRON задачи.
+
+        Этот метод подключается к CRON-таблице текущего пользователя, создает
+        новую задачу, которая будет запускать команду Django `schedule_daily_posts`
+        каждый день в полночь. Путь к `manage.py` определяется автоматически.
+
+        Args:
+            *args: Позиционные аргументы команды.
+            **kwargs: Именованные аргументы команды.
+        """
         user_cron = CronTab(user=True)
 
+        # Определяем путь к manage.py
         project_dir = Path(__file__).resolve().parent.parent.parent.parent
         manage_py = os.path.join(project_dir, 'manage.py')
 
+        # Создаем новую CRON задачу
         job = user_cron.new(command=f'python3 {manage_py} schedule_daily_posts', comment='Django Bot Posts')
 
+        # Устанавливаем расписание: ежедневно в полночь
         job.setall('0 0 * * *')
 
+        # Сохраняем изменения в CRON-таблице
         user_cron.write()
 
         self.stdout.write(self.style.SUCCESS('Successfully added CRON job to schedule daily posts'))
--- posts/management/commands/remove_cron_job.py
+++ posts/management/commands/remove_cron_job.py
@@ -3,13 +3,37 @@ from crontab import CronTab
 
 
 class Command(BaseCommand):
+    """
+    Удаляет CRON задачу для планирования ежедневных постов.
+
+    Этот класс представляет команду управления Django, которая удаляет
+    CRON задачу для планирования ежедневных постов, если таковая существует.
+    Команда ищет задачи по комментарию и удаляет их.
+
+    Attributes:
+        help (str): Описание команды для вывода в справке.
+    """
+
     help = 'Removes the CRON job for scheduling daily posts'
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **kwargs) -> None:
+        """
+        Выполняет команду удаления CRON задачи.
+
+        Этот метод подключается к CRON-таблице текущего пользователя, ищет
+        задачи с комментарием 'Django Bot Posts' и удаляет их. После этого
+        сохраняет изменения в CRON-таблице.
+
+        Args:
+            *args: Позиционные аргументы команды.
+            **kwargs: Именованные аргументы команды.
+        """
         user_cron = CronTab(user=True)
 
+        # Ищем задачу по комментарию
         user_cron.remove_all(comment='Django Bot Posts')
 
+        # Сохраняем изменения
         user_cron.write()
 
         self.stdout.write(self.style.SUCCESS('Successfully removed CRON job for scheduling daily posts'))
