840c46fca5787af0891dc2bc076d2087088966b3
Mellettan <roma23470@gmail.com>
2024-08-25 07:43:41 +0300

    refactor: add STATIC_ROOT

--- radiance/settings.py
+++ radiance/settings.py
@@ -145,6 +145,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
